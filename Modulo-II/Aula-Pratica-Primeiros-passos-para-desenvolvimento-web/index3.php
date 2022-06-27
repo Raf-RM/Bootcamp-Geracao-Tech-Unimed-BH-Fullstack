5335aeb5ca69edbe42f7c30c544f725f57aa85d1
Raf-RM <rafael.r.mateus@gmail.com>
2022-06-27 19:49:10 -0300

    Adicionando resultados para aula prática de desenvolvimento web e adicionando o Projeto da Página Pessoal

---
+++ Modulo-II/Aula-Pratica-Primeiros-passos-para-desenvolvimento-web/index3.php
@@ -0,0 +1,18 @@
+<html>
+
+	<head>
+		<title>Testes!</title>
+		
+	</head>
+	
+	<body>
+	
+		<?php
+			for	($i = 0 ; $i < 10 ; $i++){
+				print ("Linha número " . $i . "<br />");
+			}
+		?>
+	
+	</body>
+	
+</html>
